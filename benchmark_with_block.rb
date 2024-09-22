b2c36d2f28eaa1958a0a8feb19bc834722b42cad
Henry <mbahenryemeka@gmail.com>
2024-09-22 19:01:53 -0400

    benchmark with block

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,9 +1,14 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+
+  # Yield to the block to execute the code inside it
+  yield
+  end_time = Time.now 
+  duration = end_time - start_time
+  duration
 end
- 
-# Be careful, pasting this into IRB will take a long time to print.
-# It's a loooong string. :)
+
 long_string = "apple"*100000000
  
 running_time = benchmark { long_string.reverse }
